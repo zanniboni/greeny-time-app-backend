cf760a64fa81bf9d3d4ae09bea594b39d1378d69
zanniboni <viny.zanchagas@gmail.com>
2023-04-27 21:07:03 -0300

    fix: adding celebrate

--- src/Infrastructure/Routes/Users/UserRoutes.ts
+++ src/Infrastructure/Routes/Users/UserRoutes.ts
@@ -1,14 +1,61 @@
 import { Router } from 'express';
 import UserController from '../../../Adapter/Controllers/Users/UserController';
 import isAuthenticated from 'src/Domain/Middlewares/isAuthenticated';
+import { Segments, celebrate } from 'celebrate';
+import Joi from 'joi';
 
 const usersRouter = Router();
 const usersController = new UserController();
 
 usersRouter.get('/', isAuthenticated, usersController.list);
-usersRouter.post('/', usersController.create);
-usersRouter.put('/:id', usersController.update);
-usersRouter.delete('/:id', usersController.delete);
-usersRouter.get('/:id', isAuthenticated, usersController.find);
+
+usersRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  isAuthenticated,
+  usersController.find,
+);
+
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
+
+usersRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.update,
+);
+
+usersRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  usersController.delete,
+);
 
 export default usersRouter;
