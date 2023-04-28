3470a487d0c7f015b0f561a54a5cc327cad33c0c
zanniboni <viny.zanchagas@gmail.com>
2023-04-28 20:14:32 -0300

    added celebrate to all routes

--- src/Infrastructure/Routes/Categories/CategoryRoutes.ts
+++ src/Infrastructure/Routes/Categories/CategoryRoutes.ts
@@ -1,12 +1,45 @@
 import { Router } from 'express';
 import CategoryController from '../../../Adapter/Controllers/Categories/CategoryController';
 import isAuthenticated from 'src/Domain/Middlewares/isAuthenticated';
+import { Segments, celebrate } from 'celebrate';
+import Joi from 'joi';
 
 const categoryRouter = Router();
 const categoryController = new CategoryController();
 
-categoryRouter.get('/', isAuthenticated, categoryController.list);
-categoryRouter.post('/', isAuthenticated, categoryController.create);
-categoryRouter.get('/:id', isAuthenticated, categoryController.find);
+categoryRouter.get(
+  '/',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required,
+    },
+  }),
+  isAuthenticated,
+  categoryController.list,
+);
+
+categoryRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      icon: Joi.string().optional(),
+      color: Joi.string().optional(),
+    },
+  }),
+  isAuthenticated,
+  categoryController.create,
+);
+
+categoryRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required,
+    },
+  }),
+  isAuthenticated,
+  categoryController.find,
+);
 
 export default categoryRouter;
--- src/Infrastructure/Routes/Sessions/SessionRoutes.ts
+++ src/Infrastructure/Routes/Sessions/SessionRoutes.ts
@@ -1,9 +1,20 @@
 import { Router } from 'express';
 import UserSessionController from '../../../Adapter/Controllers/Sessions/SessionsController';
+import { Segments, celebrate } from 'celebrate';
+import Joi from 'joi';
 
 const sessionRouter = Router();
 const sessionController = new UserSessionController();
 
-sessionRouter.post('/', sessionController.create);
+sessionRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  sessionController.create,
+);
 
 export default sessionRouter;
